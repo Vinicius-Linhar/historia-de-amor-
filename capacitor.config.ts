a4362f5df11946c3989e373015426699c68fb6ad
unknown <vinicius.linhar1@gmail.com>
2026-02-12 12:27:34 -0300

    feat: Add Android platform support (Capacitor)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nossahistoria.app',
+  appName: 'Nossa História de Amor',
+  webDir: 'dist'
+};
+
+export default config;
